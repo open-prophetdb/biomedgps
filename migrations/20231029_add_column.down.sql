2a79796dea48fe8c9edc9e8eaa150ea3ad5218e3
Jingcheng Yang <yjcyxky@163.com>
2023-10-29 14:30:01 -0500

    Add a description column for explaining the related relation type.

---
+++ migrations/20231029_add_column.down.sql
@@ -0,0 +1,3 @@
+-- Revert: 20231029_add_column
+
+ALTER TABLE biomedgps_relation_metadata DROP COLUMN description;
---
+++ migrations/20231029_add_column.up.sql
@@ -0,0 +1,3 @@
+-- Add a description column into the biomedgps_relation_metadata table
+
+ALTER TABLE biomedgps_relation_metadata ADD COLUMN description TEXT;
